a2efaf4ac84081c39487602ebb7b1d7d6f72d67d
Asif Shahristani <shahristani.asif@gmail.com>
2022-12-04 13:07:09 +0500

    remove the social media links to amin's profiles

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,40 +1,37 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
   faLinkedin,
   faAngellist,
-} from '@fortawesome/free-brands-svg-icons';
+} from "@fortawesome/free-brands-svg-icons";
 
 const SocialMedia = () => (
-  <div className='app__social'>
+  <div className="app__social">
     <div>
-      <a href='https://github.com/aminullahdeveloper' target='_blank'>
+      <a href="https://github.com/" target="_blank">
         <FontAwesomeIcon
           icon={faGithub}
-          color='#333'
-          style={{ fontSize: '20px' }}
+          color="#333"
+          style={{ fontSize: "20px" }}
         />
       </a>
     </div>
     <div>
-      <a
-        href='https://www.linkedin.com/in/aminullah-jalal-371649211'
-        target='_blank'
-      >
+      <a href="https://www.linkedin.com/in/" target="_blank">
         <FontAwesomeIcon
           icon={faLinkedin}
-          color='#333'
-          style={{ fontSize: '20px' }}
+          color="#333"
+          style={{ fontSize: "20px" }}
         />
       </a>
     </div>
     <div>
-      <a href='https://angel.co/u/aminullah-jalal-3' target='_blank'>
+      <a href="https://angel.co/u/" target="_blank">
         <FontAwesomeIcon
           icon={faAngellist}
-          color='#333'
-          style={{ fontSize: '20px' }}
+          color="#333"
+          style={{ fontSize: "20px" }}
         />
       </a>
     </div>
